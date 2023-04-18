d820034fb3134724e87b3d77e33647aa6556de64
Aditya <adi2652002@gmail.com>
2023-04-19 04:12:57 +0530

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import First from  './components/page'
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-  <div className='text-black font-extrabold'>
-  <First id="1" 
-  num={2}/> 
+  <div className='gradient-gradual'>
+  hii gekki
+
   </div>
   )
 }
