10d37ff71a32ca3e01fe83d56e815a9694e4bcdd
Aditya Patel <86345323+Charliehexx@users.noreply.github.com>
2023-04-26 21:53:37 +0530

    Update db.ts

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -1,6 +1,6 @@
 import { Redis } from '@upstash/redis'
 
 export const db = new Redis({
-  url: "https://apn1-sweet-foxhound-33864.upstash.io",
-  token: "AYRIASQgZDJhM2ExMTctMDZlYy00NjM3LTk3MmQtMTVlMTFjYjBlOTQ3MGY2OTVkYmZjNzhmNGQ3MDk5M2MyNTE1ODZmNWZmMjE",
+  url: "https://eu2-enough-lemming-31535.upstash.io",
+  token:"AXsvASQgYmViMzY3OWMtNDdiNS00YzBkLWFlMmItNTY0NzAzNDc0NmIzMzMzMTM0YTU2NzA0NDBiNzhjNGYxMTkwM2Q3YTJlYmQ=",
 })
