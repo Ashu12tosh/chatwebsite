94a1f2b1ddf34a4b9764f77ddaf292acab4db43d
Aditya Patel <86345323+Charliehexx@users.noreply.github.com>
2023-04-26 16:37:26 +0530

    Update db.ts

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -1,6 +1,6 @@
 import { Redis } from '@upstash/redis'
 
 export const db = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
+  url: "https://apn1-sweet-foxhound-33864.upstash.io",
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 })
